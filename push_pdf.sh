bc117aac4f3bd6428d538d18f9d530122f177404
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-14 15:44:57 +0100

    Update push_pdf.sh

--- push_pdf.sh
+++ push_pdf.sh
@@ -2,4 +2,4 @@
 
 git add main.pdf
 git -c user.name='travis' -c user.email='travis' commit -m "Add built pdf"
-git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG. HEAD:master
+git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:master
