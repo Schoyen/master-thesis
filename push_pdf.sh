0717d7a473605dab36733a19df52e19fb94391ee
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-14 16:24:22 +0100

    Fix typo in script

--- push_pdf.sh
+++ push_pdf.sh
@@ -3,7 +3,7 @@
 user_name='travis'
 user_email='oyvindschoyen@gmail.com'
 
-if [[ $user_name -ne $(git log -1 --pretty=format:'%an' ]]; then
+if [[ $user_name -ne $(git log -1 --pretty=format:'%an') ]]; then
     git add main.pdf
     git -c user.name=$user_name -c user.email=$user_email commit -m "Add built pdf"
     git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:master
