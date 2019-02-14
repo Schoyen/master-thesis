7cf05ecf9803256d79861ade71ddbf230b81b804
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-14 18:11:03 +0100

    Add pdf from master branch into orhpan

--- push_pdf.sh
+++ push_pdf.sh
@@ -6,6 +6,7 @@ user_email='travis@travis-ci.org'
 if [[ "$user_name" != "$(git log -1 --pretty=format:'%an')" ]]; then
     git checkout --orphan $TRAVIS_BRANCH-pdf
     git rm -rf .
+    git checkout master main.pdf
     git add -f main.pdf
     git -c user.name="$user_name" -c user.email="$user_email" commit -m "Add built pdf"
     git push -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
