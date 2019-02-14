830f5ab4fdab1e6ff7d3adcafe46a926aa7bdfb5
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-14 17:51:07 +0100

    Use Travis mail and change branch

--- push_pdf.sh
+++ push_pdf.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-user_name='travis'
-user_email='oyvindschoyen@gmail.com'
+user_name='Travis CI'
+user_email='travis@travis-ci.org'
 
 if [[ "$user_name" != "$(git log -1 --pretty=format:'%an')" ]]; then
-    git add main.pdf
-    git -c user.name=$user_name -c user.email=$user_email commit -m "Add built pdf"
-    git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG HEAD:master
+    git checkout --orphan $TRAVIS_BRANCH-pdf
+    git rm -rf .
+    git add -f main.pdf
+    git -c user.name="$user_name" -c user.email="$user_email" commit -m "Add built pdf"
+    git push https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
