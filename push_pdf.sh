4ee3986068fc73ec5ce5bce4381dc2272fece21a
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-02-14 13:53:54 +0100

    Add script pushing built pdf from Travis

--- push_pdf.sh
+++ push_pdf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-
+git add main.pdf
+git -c user.name='travis' -c user.email='travis' commit -m "Add built pdf"
+git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG
