def4d5914cee73424ec949efaf61f9492e6f7116
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-12 16:31:04 +0200

    Add setup function for Miyagi system

--- results/benchmarks/miyagi/miyagi_system.py
+++ results/benchmarks/miyagi/miyagi_system.py
@@ -1,4 +1,9 @@
 import numpy as np
+from quantum_systems import OneDimensionalHarmonicOscillator
+from quantum_systems.time_evolution_operators import LaserField
+from quantum_systems.quantum_dots.one_dim.one_dim_potentials import (
+    AtomicPotential,
+)
 
 
 class MiyagiLaser:
@@ -16,6 +21,33 @@ class MiyagiLaser:
         term += 2 * np.pi / T * np.cos(np.pi * t / T) * np.sin(omega * t)
 
         return -np.sin(np.pi * t / T) * term
+
+
+def get_miyagi_system(n=4, l=30):
+    # One-dimensional atomic system as in the article by Miyagi and Madsen.
+    Z = n  # Number of protons
+    c = 1  # Position of nucleus
+    a = 1  # Shielded Columb parameter
+    alpha = 1  # Shielded Columb parameter
+
+    grid_length = 300
+    num_grid_points = 5001
+
+    potential = AtomicPotential(Za=Z, c=c)
+    laser = MiyagiLaser()
+
+    odbe = OneDimensionalHarmonicOscillator(
+        n,
+        l,
+        grid_length=grid_length,
+        num_grid_points=num_grid_points,
+        a=a,
+        alpha=alpha,
+    )
+    odbe.setup_system(potential=potential)
+    odbe.set_time_evolution_operator(LaserField(laser))
+
+    return odbe
 
 
 if __name__ == "__main__":
