c899b3e5c79a5698ca3a87b1c44252d0754b8b70
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-19 11:45:51 +0200

    Output time including rhs solution
        Also, use GaussIntegrator.

--- results/benchmarks/nest/ccd_nest.py
+++ results/benchmarks/nest/ccd_nest.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import datetime
 
 import numpy as np
 
@@ -15,7 +16,9 @@ path = os.path.join(sys.path[0], "dat")
 
 lih = get_lih_system()
 
-oatdccd = OATDCCD(lih, verbose=True)
+integrator = GaussIntegrator(s=3, eps=1e-6, np=np)
+
+oatdccd = OATDCCD(lih, integrator=integrator, verbose=True)
 oatdccd.compute_ground_state(tol=1e-8, termination_tol=1e-8)
 oatdccd.set_initial_conditions()
 
@@ -37,30 +40,33 @@ rho = oatdccd.compute_one_body_density_matrix()
 dipole_x[0] = np.trace(rho @ x)
 dipole_z[0] = np.trace(rho @ x)
 energy[0] = oatdccd.compute_energy()
-t_1 = time.time()
 
 time_accumulator = 0
 
 
 try:
     for i, amp in enumerate(oatdccd.solve(time_points)):
+        t_1 = time.time()
 
         time_accumulator += t_1 - t_0
 
-        if i % 10:
-            print(f"Iteration: {i + 1} / {num_steps} (time: {t_1 - t_0} sec)")
+        if i % 10 == 0:
+            print(f"Iteration: {i + 1} / {num_steps}")
 
             time_per_iteration = time_accumulator / (i + 1)
             time_left = (num_steps - (i + 1)) * time_per_iteration
+            time_remaining = datetime.timedelta(seconds=time_left)
             print(
                 f"""
-            Total run time: {time_accumulator}
-            Time per iteration: {time_per_iteration}
-            Time remaining: {time_left}
+            Total run time: {time_accumulator} sec
+            Time per iteration: {time_per_iteration} sec
+            Time remaining: {time_left} sec
+            Time remaining: {time_remaining}
             """
             )
 
         t_0 = time.time()
+
         t, l, C, C_tilde = amp
 
         x = C_tilde @ lih.dipole_moment[0] @ C
@@ -71,7 +77,6 @@ try:
         dipole_x[i + 1] = np.trace(rho @ x)
         dipole_z[i + 1] = np.trace(rho @ x)
         energy[i + 1] = oatdccd.compute_energy()
-        t_1 = time.time()
 
 except Exception:
     # Save data in case of crash
