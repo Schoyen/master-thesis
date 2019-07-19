e77a6c4b3b7f3a15873e2e7bea80ca4ea91dd1a1
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-19 11:48:02 +0200

    Add jobscript for long Nest simulation

---
+++ results/benchmarks/nest/jobscript.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Job name
+#SBATCH --job-name=benchmark-thesis-nest
+#
+# Project
+#SBATCH --account=nn2977k
+#
+# Wall clock limit
+#SBATCH --time=96:00:00
+#
+# Max memory usage per core (MB)
+#SBATCH --mem-per-cpu=4G
+#
+# Number of CPU's/processes
+#SBATCH --cpus-per-task=8
+
+## Set up job environment
+source /cluster/bin/jobsetup
+module purge
+set -o errexit
+
+## Copy files to work directory
+cp $SUBMITDIR/*.py $SCRATCH
+
+## Create directory for data
+mkdir $SCRATCH/dat
+
+## Mark outfiles for copying
+cleanup "cp $SCRATCH/dat/* $SUBMITDIR/dat/"
+
+## Run commands
+cd $SCRATCH
+export OMP_NUM_THREADS=8
+python ccd_nest.py
