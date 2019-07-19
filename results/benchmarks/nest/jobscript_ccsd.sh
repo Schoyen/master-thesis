79a470dc9b76ef53ea598f197e13d1f9dfbf02b7
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-19 18:13:32 +0200

    Add jobscript for TDCCSD

---
+++ results/benchmarks/nest/jobscript_ccsd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Job name
+#SBATCH --job-name=benchmark-thesis-nest-tdccsd
+#
+# Project
+#SBATCH --account=nn2977k
+#
+# Wall clock limit
+#SBATCH --time=48:00:00
+#
+# Max memory usage per core (MB)
+#SBATCH --mem-per-cpu=4G
+#
+# Number of CPU's/processes
+#SBATCH --cpus-per-task=2
+
+## Set up job environment
+source /cluster/bin/jobsetup
+module purge
+set -o errexit
+
+## Copy files to work directory
+cp $SUBMITDIR/*.py $SCRATCH
+
+## Create directory for data
+mkdir $SCRATCH/dat
+
+## Mark outfiles for copying
+cleanup "cp $SCRATCH/dat/* $SUBMITDIR/dat/"
+
+## Run commands
+cd $SCRATCH
+export OMP_NUM_THREADS=2
+python ccsd_nest.py
