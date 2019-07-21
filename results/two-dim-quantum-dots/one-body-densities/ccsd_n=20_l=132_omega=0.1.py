3912159195550fab4a609b9ba1d4e2d7e0da774f
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-21 15:10:34 +0200

    Add scripts for n=20

---
+++ results/two-dim-quantum-dots/one-body-densities/ccsd_n=20_l=132_omega=0.1.py
@@ -0,0 +1,5 @@
+from ccsd_tdho import get_filename_stub, run_ccsd_tdho
+
+params = dict(n=20, l=132, radius_length=17, num_grid_points=101, omega=0.1)
+filename_stub = get_filename_stub(params)
+run_ccsd_tdho(params, filename_stub)
