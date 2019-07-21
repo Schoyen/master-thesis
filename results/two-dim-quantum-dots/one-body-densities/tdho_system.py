cbdaa4efaba3b90a074dd9dd1b3043bb7c8d4857
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-21 14:40:56 +0200

    Accept all args and kwargs

--- results/two-dim-quantum-dots/one-body-densities/tdho_system.py
+++ results/two-dim-quantum-dots/one-body-densities/tdho_system.py
@@ -6,11 +6,8 @@ from quantum_systems import TwoDimensionalHarmonicOscillator
 os.environ["QS_CACHE_TDHO"] = "1"
 
 
-def get_tdho(n=2, l=12):
-    radius = 3
-    num_grid_points = 201
-
-    tdho = TwoDimensionalHarmonicOscillator(n, l, radius, num_grid_points)
+def get_tdho(*args, **kwargs):
+    tdho = TwoDimensionalHarmonicOscillator(*args, **kwargs)
     tdho.setup_system(verbose=True)
 
     return tdho
