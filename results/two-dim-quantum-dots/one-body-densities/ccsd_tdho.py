ff2bbb97156d43323643c0c934833810f6ba5e7f
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-21 14:41:10 +0200

    Add script running HF and CCSD solvers

---
+++ results/two-dim-quantum-dots/one-body-densities/ccsd_tdho.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tdho_system import get_tdho
+from coupled_cluster.ccsd import CoupledClusterSinglesDoubles
+from hartree_fock import HartreeFock
+
+
+def get_filename_stub(params):
+    return f"ccsd_n={params['n']}_l={params['l']}_omega={params['omega']}"
+
+
+def run_ccsd_tdho(params, filename_stub):
+    tdho = get_tdho(**params)
+
+    for tol in [1e-8, 1e-6, 1e-4]:
+        try:
+            hf = HartreeFock(tdho, verbose=True)
+            hf.compute_ground_state(tol=tol, change_system_basis=True)
+            break
+        except AssertionError:
+            pass
+
+    for tol in [1e-8, 1e-6, 1e-4]:
+        try:
+            ccsd = CoupledClusterSinglesDoubles(tdho, verbose=True)
+            ccsd.compute_ground_state(
+                t_kwargs=dict(tol=1e-8), l_kwargs=dict(tol=1e-8)
+            )
+            break
+        except AssertionError:
+            pass
+
+    rho = ccsd.compute_particle_density()
+
+    path = os.path.join(sys.path[0], "dat")
+    filename = filename_stub + "_rho_real.dat"
+    filename = os.path.join(path, filename)
+
+    np.savetxt(
+        filename,
+        np.c_[
+            tdho.T.ravel()[:, np.newaxis],
+            tdho.R.ravel()[:, np.newaxis],
+            rho.real.ravel()[:, np.newaxis],
+        ],
+    )
+
+    fig = plt.figure()
+    fig.add_subplot(1, 1, 1, polar=True)
+
+    plt.contourf(tdho.T, tdho.R, rho.real)
+    plt.savefig(os.path.join(path, filename_stub + "_rho_real.pdf"))
