b9f5088bcb45e4cd3d2331e2002b342a73132efa
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-22 18:42:09 +0200

    Add duplice files with l=90

---
+++ results/two-dim-quantum-dots/one-body-densities/ccsd_n=2_l=90_omega=0.28.py
@@ -0,0 +1,5 @@
+from ccsd_tdho import get_filename_stub, run_ccsd_tdho
+
+params = dict(n=2, l=90, radius_length=4, num_grid_points=101, omega=0.28)
+filename_stub = get_filename_stub(params)
+run_ccsd_tdho(params, filename_stub)
