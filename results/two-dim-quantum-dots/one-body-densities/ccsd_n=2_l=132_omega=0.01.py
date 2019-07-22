76fe99566ad6976b9ff7b020f8a72ed2e6b05077
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-22 13:48:23 +0200

    Increase tolerance of Hartree-Fock

--- results/two-dim-quantum-dots/one-body-densities/ccsd_n=2_l=132_omega=0.01.py
+++ results/two-dim-quantum-dots/one-body-densities/ccsd_n=2_l=132_omega=0.01.py
@@ -1,5 +1,5 @@
 from ccsd_tdho import get_filename_stub, run_ccsd_tdho
 
-params = dict(n=2, l=132, radius_length=30, num_grid_points=101, omega=0.01)
+params = dict(n=2, l=132, radius_length=30, num_grid_points=201, omega=0.01)
 filename_stub = get_filename_stub(params)
-run_ccsd_tdho(params, filename_stub)
+run_ccsd_tdho(params, filename_stub, hf_tol=1e-3)
