3e9f04d7f38db974f63b9ddb6fe0ca21769e4248
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-24 16:27:39 +0200

    Add jobscript for large cache

--- results/two-dim-quantum-dots/one-body-densities/jobscript_cache.sh
+++ results/two-dim-quantum-dots/one-body-densities/jobscript_cache.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Job name
-#SBATCH --job-name=two-dim-cache
+#SBATCH --job-name=two-dim-cache-210
 #
 # Project
 #SBATCH --account=nn2977k
 #
 # Wall clock limit
-#SBATCH --time=04:00:00
+#SBATCH --time=10:00:00
 #
 # Max memory usage per core (MB)
 #SBATCH --mem-per-cpu=15300
@@ -23,17 +23,10 @@ set -o errexit
 ## Copy files to work directory
 cp $SUBMITDIR/*.py $SCRATCH
 
-## Copy cached Coulomb elements to work directory
-cp $SUBMITDIR/*.npy $SCRATCH
-
-## Create directory for data
-mkdir $SCRATCH/dat
-
 ## Mark outfiles for copying
-cleanup "cp $SCRATCH/dat/* $SUBMITDIR/dat/"
 cleanup "cp $SCRATCH/*.npy $SUBMITDIR/"
 
 ## Run commands
 cd $SCRATCH
 export OMP_NUM_THREADS=4
-python -u setup_large_system.py | tee $SCRATCH/dat/setup_large_system.log
+python setup_large_system.py 210
