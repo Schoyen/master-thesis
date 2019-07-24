f6a8ada293fa857759f94dc985e673eb9984d063
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-24 15:47:17 +0200

    Accept number of spin-orbitals on command-line

--- results/two-dim-quantum-dots/one-body-densities/setup_large_system.py
+++ results/two-dim-quantum-dots/one-body-densities/setup_large_system.py
@@ -1,4 +1,18 @@
+import sys
+import numpy as np
 from tdho_system import cache_large_system
 
 
-cache_large_system(n=2, l=90, radius_length=10, num_grid_points=201)
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <l>")
+    sys.exit()
+
+l = int(sys.argv[1])
+shells = list((np.cumsum(np.arange(20)) * 2).astype(int))
+
+if l not in shells:
+    print(f"l = {l} not in {shells}")
+    sys.exit()
+
+print(f"Starting caching for l = {l}")
+cache_large_system(n=2, l=l, radius_length=10, num_grid_points=201)
