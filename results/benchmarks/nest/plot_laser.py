3512f6f75f26a3afda472666e33746d40be1fe26
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-26 23:07:13 +0200

    Add fft of dipole in z-direction from Nest

---
+++ results/benchmarks/nest/plot_laser.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tdqd_tools.io_data import write_data
+from lih_nest_system import NestLaser, get_time_points
+
+
+laser = np.vectorize(NestLaser())
+time_points = get_time_points()
+
+time_points = time_points[time_points <= 50]
+
+
+path = os.path.join(sys.path[0], "dat")
+write_data(
+    os.path.join(path, "nest_laser.dat"), time_points, laser(time_points)
+)
