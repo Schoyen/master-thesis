587456edd3c527191d1e53c7957efe94a7f0674a
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-07-27 13:15:43 +0200

    Add run using n=6, l=182, and omega=0.1

--- results/two-dim-quantum-dots/one-body-densities/jobscript.sh
+++ results/two-dim-quantum-dots/one-body-densities/jobscript.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Job name
-#SBATCH --job-name=two-dim-densities-ccsd-6-156
+#SBATCH --job-name=two-dim-densities-ccsd-6-182
 #
 # Project
 #SBATCH --account=nn2977k
 #
 # Wall clock limit
-#SBATCH --time=10:00:00
+#SBATCH --time=20:00:00
 #
 # Max memory usage per core (MB)
 #SBATCH --mem-per-cpu=15300
@@ -35,4 +35,4 @@ cleanup "cp $SCRATCH/dat/* $SUBMITDIR/dat/"
 ## Run commands
 cd $SCRATCH
 export OMP_NUM_THREADS=4
-python -u ccsd_n=6_l=156_omega=0.1.py | tee $SCRATCH/dat/ccsd_n=6_l=156_omega=0.1.log
+python -u ccsd_n=6_l=182_omega=0.1.py | tee $SCRATCH/dat/ccsd_n=6_l=182_omega=0.1.log
