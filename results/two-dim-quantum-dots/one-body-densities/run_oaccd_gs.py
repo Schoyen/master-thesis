b99a4208cefc87050a53fe01f831ca60bf7009d2
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-09 18:36:24 +0200

    Add results for n=6,l=20,30 and omega=0.01

--- results/two-dim-quantum-dots/one-body-densities/run_oaccd_gs.py
+++ results/two-dim-quantum-dots/one-body-densities/run_oaccd_gs.py
@@ -5,7 +5,8 @@ from oaccd_tdho import get_filename_stub, run_oaccd_tdho
 
 if len(sys.argv) < 6:
     print(
-        f"Usage: {sys.argv[0]} <n> <l> <radius_length> <num_grid_points> <omega>"
+        f"Usage: {sys.argv[0]} <n> <l> <radius_length> <num_grid_points> "
+        + "<omega> <optional hf_tol> <optional oaccd_tol"
     )
     sys.exit()
 
@@ -14,6 +15,9 @@ l = int(sys.argv[2])
 radius_length = float(sys.argv[3])
 num_grid_points = int(sys.argv[4])
 omega = float(sys.argv[5])
+
+hf_tol = 1e-7 if len(sys.argv) == 6 else float(sys.argv[6])
+oaccd_tol = 1e-4 if len(sys.argv) == 7 else float(sys.argv[7])
 
 
 params = dict(
@@ -24,4 +28,4 @@ params = dict(
     omega=omega,
 )
 filename_stub = get_filename_stub(params)
-run_oaccd_tdho(params, filename_stub)
+run_oaccd_tdho(params, filename_stub, hf_tol=hf_tol, oaccd_tol=oaccd_tol)
