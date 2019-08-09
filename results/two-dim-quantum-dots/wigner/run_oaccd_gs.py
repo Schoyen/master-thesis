4b12f974190fefd052e5b4a0e2c22d0a8bdac701
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-09 21:17:09 +0200

    Add system setup and store log-files

--- results/two-dim-quantum-dots/wigner/run_oaccd_gs.py
+++ results/two-dim-quantum-dots/wigner/run_oaccd_gs.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import logging
 
 from oaccd_tdho import get_filename_stub, run_oaccd_tdho
 
@@ -16,8 +18,8 @@ radius_length = float(sys.argv[3])
 num_grid_points = int(sys.argv[4])
 omega = float(sys.argv[5])
 
-hf_tol = 1e-7 if len(sys.argv) == 6 else float(sys.argv[6])
-oaccd_tol = 1e-4 if len(sys.argv) == 7 else float(sys.argv[7])
+hf_tol = 1e-7 if len(sys.argv) <= 6 else float(sys.argv[6])
+oaccd_tol = 1e-4 if len(sys.argv) <= 7 else float(sys.argv[7])
 
 
 params = dict(
@@ -28,4 +30,13 @@ params = dict(
     omega=omega,
 )
 filename_stub = get_filename_stub(params)
+
+path = os.path.join(sys.path[0], "dat")
+log_name = "oaccd_" + filename_stub + ".log"
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler(os.path.join(path, log_name), "a"))
+print = logger.info
+
 run_oaccd_tdho(params, filename_stub, hf_tol=hf_tol, oaccd_tol=oaccd_tol)
