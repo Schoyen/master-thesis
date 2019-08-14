53ae23c85a7148242355403c181a6748a274edd4
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-14 17:58:07 +0200

    Continue on size-consistency in CI

--- results/one-dim-quantum-dots/size-consistency/size.py
+++ results/one-dim-quantum-dots/size-consistency/size.py
@@ -7,28 +7,45 @@ from coupled_cluster.ccsd import CoupledClusterSinglesDoubles as CCSD
 from hartree_fock import HartreeFock
 
 n = 2
-l = 20
+l = 4
+l_sq = int(l ** 2)
 
 odqd_ci = ODQD(n, l, 40, 201, alpha=0.1)
 odqd_ci.setup_system()
 odqd_ci._h = np.kron(odqd_ci._h, np.eye(l)) + np.kron(np.eye(l), odqd_ci._h)
-odqd_ci._u.fill(0)
+odqd_ci._u = (
+    np.kron(odqd_ci._u.reshape(l_sq, l_sq), np.eye(l_sq))
+    + np.kron(np.eye(l_sq), odqd_ci._u.reshape(l_sq, l_sq))
+).reshape(l_sq, l_sq, l_sq, l_sq)
+odqd_ci.set_system_size(2 * n, l_sq)
 
 odqd_cc = ODQD(n, l, 40, 201, alpha=0.1)
 odqd_cc.setup_system()
-# odqd_cc._u.fill(0)
+odqd_cc._h = np.kron(odqd_cc._h, np.eye(l)) + np.kron(np.eye(l), odqd_cc._h)
+odqd_cc._u = (
+    np.kron(odqd_cc._u.reshape(l_sq, l_sq), np.eye(l_sq))
+    + np.kron(np.eye(l_sq), odqd_cc._u.reshape(l_sq, l_sq))
+).reshape(l_sq, l_sq, l_sq, l_sq)
+odqd_cc.set_system_size(2 * n, l_sq)
+
 
 odqd_hf = ODQD(n, l, 40, 201, alpha=0.1)
 odqd_hf.setup_system()
-# odqd_hf._u.fill(0)
+odqd_hf._h = np.kron(odqd_hf._h, np.eye(l)) + np.kron(np.eye(l), odqd_hf._h)
+odqd_hf._u = (
+    np.kron(odqd_hf._u.reshape(l_sq, l_sq), np.eye(l_sq))
+    + np.kron(np.eye(l_sq), odqd_hf._u.reshape(l_sq, l_sq))
+).reshape(l_sq, l_sq, l_sq, l_sq)
+odqd_hf._s = np.kron(odqd_hf._s, np.eye(l)) + np.kron(np.eye(l), odqd_hf._s)
+odqd_hf.set_system_size(2 * n, l_sq)
 
 cisd = CISD(odqd_ci, verbose=True)
 cisd.compute_ground_state()
 
-ccsd = CCSD(odqd_cc, verbose=True)
-ccsd.compute_ground_state(
-    t_kwargs=dict(num_vecs=20), l_kwargs=dict(num_vecs=20)
-)
+#ccsd = CCSD(odqd_cc, verbose=True)
+#ccsd.compute_ground_state(
+#    t_kwargs=dict(num_vecs=20), l_kwargs=dict(num_vecs=20)
+#)
 
 hf = HartreeFock(odqd_hf, verbose=True)
 hf.compute_ground_state()
