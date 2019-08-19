81f42e3eb2708394c1d6036330192fa0b8150135
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-19 19:58:56 +0200

    Start on one-body densities

---
+++ results/quantum-dots/two-dim-quantum-dots/one-body-densities/run_oaccd_gs.py
@@ -0,0 +1,31 @@
+import sys
+
+from oaccd_tdho import get_filename_stub, run_oaccd_tdho
+
+
+if len(sys.argv) < 6:
+    print(
+        f"Usage: {sys.argv[0]} <n> <l> <radius_length> <num_grid_points> "
+        + "<omega> <optional hf_tol> <optional oaccd_tol"
+    )
+    sys.exit()
+
+n = int(sys.argv[1])
+l = int(sys.argv[2])
+radius_length = float(sys.argv[3])
+num_grid_points = int(sys.argv[4])
+omega = float(sys.argv[5])
+
+hf_tol = 1e-4 if len(sys.argv) < 7 else float(sys.argv[6])
+oaccd_tol = 1e-4 if len(sys.argv) < 8 else float(sys.argv[7])
+
+
+params = dict(
+    n=n,
+    l=l,
+    radius_length=radius_length,
+    num_grid_points=num_grid_points,
+    omega=omega,
+)
+filename_stub = get_filename_stub(params)
+run_oaccd_tdho(params, filename_stub, hf_tol=hf_tol, oaccd_tol=oaccd_tol)
