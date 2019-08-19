5ef545d8532bb0d42322ed0d5c9ef5cdfadcb73a
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-19 17:51:56 +0200

    Add jobscripts

---
+++ results/benchmarks/isborn/jobscript_lih_hf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Job name
+#SBATCH --job-name=lih-isborn-hf
+#
+# Project
+#SBATCH --account=nn2977k
+#
+# Wall clock limit
+#SBATCH --time=96:00:00
+#
+# Max memory usage per core (MB)
+#SBATCH --mem-per-cpu=4G
+#
+# Number of CPU's/processes
+#SBATCH --cpus-per-task=4
+
+## Set up job environment
+source /cluster/bin/jobsetup
+module purge
+set -o errexit
+
+## Copy files to work directory
+cp $SUBMITDIR/*.py $SCRATCH
+
+## Create directory for data
+mkdir $SCRATCH/dat
+
+## Mark outfiles for copying
+cleanup "cp $SCRATCH/dat/* $SUBMITDIR/dat/"
+
+## Run commands
+cd $SCRATCH
+export OMP_NUM_THREADS=4
+python run_hf lih 6-31gss both
+python run_hf lih 6-31gss up
