10812c5385578370454ca314069d3566354d312e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-31 11:16:16 +0200

    Add output of all transforms

--- results/atoms-and-molecules/time-evolution-noble-gasses/dat/peak_finder.py
+++ results/atoms-and-molecules/time-evolution-noble-gasses/dat/peak_finder.py
@@ -5,30 +5,38 @@ import pint
 
 from tdqd_tools.io_data import return_time_data
 
-solvers = ["oatdccd"]
-basis_sets = ["aug-ccpvdz"]
+atoms = ["ne", "ar"]
+solvers = ["oatdccd", "tdccsd"]
+basis_sets = ["ccpvdz", "aug-ccpvdz"]
 
 ureg = pint.UnitRegistry()
 
-for basis in basis_sets:
-    for solver in solvers:
-        filename = f"fft_ne_{basis}_{solver}_new_real.dat"
+for atom in atoms:
+    print("-" * 46 + f" {atom} " + "-" * 46)
+    for basis in basis_sets:
+        print(basis)
+        for solver in solvers:
+            filename = f"fft_{atom}_{basis}_{solver}_new_real.dat"
 
-        data = return_time_data(filename)
-        time, spec = data[:, 0], data[:, 1]
+            try:
+                data = return_time_data(filename)
+            except Exception:
+                continue
 
-        peak_indices = scipy.signal.find_peaks(spec, height=0.01)
+            time, spec = data[:, 0], data[:, 1]
 
-        peaks = list(time[peak_indices[0]])
+            peak_indices = scipy.signal.find_peaks(spec, height=0.01)
 
-        for i, peak in enumerate(peaks):
-            energy_au = peak
-            energy_ev = (peak * ureg.hartree).to(ureg.eV).magnitude
+            peaks = list(time[peak_indices[0]])
 
-            tab_entry = (
-                rf"& ${i + 1}$ & ${energy_au:.4f}$ & ${energy_ev:.4f}$ \\"
-            )
-            if i == 0:
-                tab_entry = solver.upper() + " " + tab_entry
+            for i, peak in enumerate(peaks):
+                energy_au = peak
+                energy_ev = (peak * ureg.hartree).to(ureg.eV).magnitude
 
-            print(tab_entry)
+                tab_entry = (
+                    rf"& ${i + 1}$ & ${energy_au:.4f}$ & ${energy_ev:.4f}$ \\"
+                )
+                if i == 0:
+                    tab_entry = solver.upper() + " " + tab_entry
+
+                print(tab_entry)
