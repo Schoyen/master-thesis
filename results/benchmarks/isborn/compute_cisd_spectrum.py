ba1ebdf535a1600af9857e7acae359892ef4b309
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-08-31 21:48:54 +0200

    Add transitions for LiH

--- results/benchmarks/isborn/compute_cisd_spectrum.py
+++ results/benchmarks/isborn/compute_cisd_spectrum.py
@@ -2,12 +2,12 @@ import pint
 import numpy as np
 import matplotlib.pyplot as plt
 
-from isborn_system import get_h2_system
+from isborn_system import get_h2_system, get_lih_system
 
 from configuration_interaction import CISD, CIS
 
-h2_no = get_h2_system(spin_dependent=False, basis="6-31gss")
-h2_yes = get_h2_system(spin_dependent=True, basis="6-31gss")
+h2_no = get_lih_system(spin_dependent=False, basis="6-31gss")
+h2_yes = get_lih_system(spin_dependent=True, basis="6-31gss")
 
 cisd_no = CISD(h2_no, verbose=True)
 cisd_no.compute_ground_state()
@@ -18,8 +18,8 @@ cisd_yes.compute_ground_state()
 ureg = pint.UnitRegistry()
 
 
-for J in range(len(cisd_no.energies)):
-    if cisd_no.energies[J] - cisd_no.energies[0] > 1.0:
+for J in range(1, len(cisd_no.energies)):
+    if cisd_no.energies[J] - cisd_no.energies[0] > 0.5:
         break
 
     allowed_dipole = cisd_no.allowed_dipole_transition(0, J)
@@ -46,13 +46,13 @@ for J in range(len(cisd_no.energies)):
     tab_entry = rf"$0 \to {J}$ & {singlet_allowed} & ${energy_au:.4f}$ & ${energy_ev:.4f}$ \\"
     print(tab_entry)
 
-    #print(f"J = {J}")
-    #print("Spin-independent:")
-    #print(cisd_no.allowed_dipole_transition(0, J))
-    #print(cisd_no.energies[J] - cisd_no.energies[0])
+    # print(f"J = {J}")
+    # print("Spin-independent:")
+    # print(cisd_no.allowed_dipole_transition(0, J))
+    # print(cisd_no.energies[J] - cisd_no.energies[0])
 
-    #print("---")
-    #print("Spin-dependent")
-    #print(cisd_yes.allowed_dipole_transition(0, J))
-    #print(cisd_yes.energies[J] - cisd_yes.energies[0])
-    #print("=" * 100)
+    # print("---")
+    # print("Spin-dependent")
+    # print(cisd_yes.allowed_dipole_transition(0, J))
+    # print(cisd_yes.energies[J] - cisd_yes.energies[0])
+    # print("=" * 100)
