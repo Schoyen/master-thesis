b35a10c454e8048b8ce690a86e08d5ff081540e5
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-09-03 19:31:24 +0200

    Add output of ionization

--- results/benchmarks/miyagi/ccd_miyagi.py
+++ results/benchmarks/miyagi/ccd_miyagi.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import tqdm
 
 from miyagi_system import get_miyagi_system
 from hartree_fock import HartreeFock
@@ -11,7 +12,12 @@ from tdqd_tools.io_data import write_data
 
 path = os.path.join(sys.path[0], "dat")
 
-l = 40
+
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <l>")
+    sys.exit()
+
+l = int(sys.argv[1])
 
 be = get_miyagi_system(n=4, l=l)
 
@@ -24,11 +30,15 @@ oatdccd = OATDCCD(be, integrator=integrator, verbose=True)
 oatdccd.compute_ground_state(tol=1e-10, termination_tol=1e-10)
 oatdccd.set_initial_conditions()
 
+nuc_mask = np.abs(be.grid) <= 20
+nucleus = be.grid[nuc_mask]
 
 rho = oatdccd.compute_particle_density()
 write_data(
     os.path.join(path, f"rho_oatdccd_start_real_l={l}.dat"), be.grid, rho.real
 )
+
+norm = np.trapz(rho.real, be.grid)
 
 
 t_start = 0
@@ -38,17 +48,21 @@ dt = 1e-2
 num_timesteps = int((t_end - t_start) / dt + 1)
 time_points = np.linspace(t_start, t_end, num_timesteps)
 
+ionization = np.zeros(num_timesteps)
+ionization[0] = np.trapz(rho[nuc_mask].real, be.grid[nuc_mask]) / norm
+
 tol = dt / 100
 
 T_half = t_end / 2
 T = t_end
 
-for i, amp in enumerate(oatdccd.solve(time_points)):
-    if i % 10 == 0:
-        print(f"{i + 1} / {num_timesteps}")
+for i, amp in tqdm.tqdm(
+    enumerate(oatdccd.solve(time_points)), total=num_timesteps - 1
+):
+    rho = oatdccd.compute_particle_density()
+    ionization[i + 1] = np.trapz(rho[nuc_mask].real, be.grid[nuc_mask]) / norm
 
     if abs(time_points[i] - T_half) < tol:
-        rho = oatdccd.compute_particle_density()
         write_data(
             os.path.join(path, f"rho_oatdccd_half_real_l={l}.dat"),
             be.grid,
@@ -58,4 +72,10 @@ for i, amp in enumerate(oatdccd.solve(time_points)):
 rho = oatdccd.compute_particle_density()
 write_data(
     os.path.join(path, f"rho_oatdccd_end_real_l={l}.dat"), be.grid, rho.real
+)
+
+write_data(
+    os.path.join(path, f"ionization_oatdccd_real_l={l}.dat"),
+    time_points,
+    ionization,
 )
--- results/benchmarks/miyagi/hf_miyagi.py
+++ results/benchmarks/miyagi/hf_miyagi.py
@@ -11,7 +11,11 @@ from tdqd_tools.io_data import write_data
 
 path = os.path.join(sys.path[0], "dat")
 
-l = 40
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <l>")
+    sys.exit()
+
+l = int(sys.argv[1])
 
 be = get_miyagi_system(n=4, l=l)
 integrator = GaussIntegrator(s=3, eps=1e-6, np=np)
@@ -19,20 +23,24 @@ tdhf = TDHF(be, integrator=integrator, verbose=True)
 tdhf.compute_ground_state(tol=1e-5)
 tdhf.set_initial_conditions()
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+#
+# plt.plot(be.grid, be.potential(be.grid))
+#
+# for i in range(l // 2):
+#    plt.plot(be.grid, be.spf[i * 2].real + be.eigen_energies[i])
+#
+# plt.show()
 
-plt.plot(be.grid, be.potential(be.grid))
-
-for i in range(l // 2):
-    plt.plot(be.grid, be.spf[i * 2].real + be.eigen_energies[i])
-
-plt.show()
-
+nuc_mask = np.abs(be.grid) <= 20
+nucleus = be.grid[nuc_mask]
 
 rho = tdhf.compute_particle_density()
 write_data(
     os.path.join(path, f"rho_tdhf_start_real_l={l}.dat"), be.grid, rho.real
 )
+
+norm = np.trapz(rho.real, be.grid)
 
 
 t_start = 0
@@ -42,6 +50,9 @@ dt = 1e-2
 num_timesteps = int((t_end - t_start) / dt + 1)
 time_points = np.linspace(t_start, t_end, num_timesteps)
 
+ionization = np.zeros(num_timesteps)
+ionization[0] = np.trapz(rho[nuc_mask].real, be.grid[nuc_mask]) / norm
+
 tol = dt / 100
 
 T_half = t_end / 2
@@ -50,6 +61,9 @@ T = t_end
 for i, amp in tqdm.tqdm(
     enumerate(tdhf.solve(time_points)), total=num_timesteps - 1
 ):
+    rho = tdhf.compute_particle_density()
+    ionization[i + 1] = np.trapz(rho[nuc_mask].real, be.grid[nuc_mask]) / norm
+
     if abs(time_points[i] - T_half) < tol:
         rho = tdhf.compute_particle_density()
         write_data(
@@ -61,4 +75,10 @@ for i, amp in tqdm.tqdm(
 rho = tdhf.compute_particle_density()
 write_data(
     os.path.join(path, f"rho_tdhf_end_real_l={l}.dat"), be.grid, rho.real
+)
+
+write_data(
+    os.path.join(path, f"ionization_tdhf_real_l={l}.dat"),
+    time_points,
+    ionization,
 )
