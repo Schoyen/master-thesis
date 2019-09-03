a570cd40e97a5b86438dc204f01ca9328e56d95e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-09-03 20:08:24 +0200

    Use fewer cores to jump the queue

--- results/benchmarks/miyagi/jobscript.sh
+++ results/benchmarks/miyagi/jobscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Job name
-#SBATCH --job-name=ion-20-hf
+#SBATCH --job-name=ion-20
 #
 # Project
 #SBATCH --account=nn2977k
@@ -13,7 +13,7 @@
 #SBATCH --mem-per-cpu=4G
 #
 # Number of CPU's/processes
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 
 ## Set up job environment
 source /cluster/bin/jobsetup
@@ -32,13 +32,13 @@ cleanup "cp $SCRATCH/dat/* $SUBMITDIR/dat/"
 
 ## Run commands
 cd $SCRATCH
-export OMP_NUM_THREADS=8
-#python ccd_miyagi.py 20
+export OMP_NUM_THREADS=4
+python ccd_miyagi.py 20
 #python ccd_miyagi.py 30
 #python ccd_miyagi.py 36
 #python ccd_miyagi.py 40
 
-python hf_miyagi.py 20
+#python hf_miyagi.py 20
 #python hf_miyagi.py 30
 #python hf_miyagi.py 36
 #python hf_miyagi.py 40
