32d86e3fabc98a455fe10d51a71bdb02514a98e7
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2019-09-24 07:52:51 +0200

    Add demonstration script

--- results/quantum-dots/one-dim-quantum-dots/hpt/odho_system.py
+++ results/quantum-dots/one-dim-quantum-dots/hpt/odho_system.py
@@ -2,21 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-from hpt_system import get_hpt_system
-
-from configuration_interaction import TDCISD
-from coupled_cluster.ccd import OATDCCD
-from hartree_fock import HartreeFock
-
-
-n = 2
-l = 20
-
-omega = 1
-
-grid_length = 10
-num_grid_points = 401
-
 from quantum_systems import ODQD
 from quantum_systems.quantum_dots.one_dim.one_dim_potentials import (
     HOPotential,
@@ -24,8 +9,20 @@ from quantum_systems.quantum_dots.one_dim.one_dim_potentials import (
 )
 from quantum_systems.time_evolution_operators import LaserField
 
+from configuration_interaction import TDCISD
+
+
+n = 2
+l = 12
+
+omega = 1
+
+grid_length = 10
+num_grid_points = 401
+
+
 odqd = ODQD(n, l, grid_length, num_grid_points)
-#odqd.setup_system(potential=DWPotential(omega=omega, l=2))
+# odqd.setup_system(potential=DWPotential(omega=omega, l=2))
 odqd.setup_system(potential=HOPotential(omega=omega))
 
 
@@ -36,9 +33,6 @@ laser_pulse = lambda t: E * np.sin(laser_omega * t)
 laser_envelope = lambda t, T: np.sin(np.pi * t / T) ** 2 if t <= T else 0
 laser = lambda t: laser_envelope(t, 5) * laser_pulse(t)
 odqd.set_time_evolution_operator(LaserField(laser))
-
-# odqd = get_hpt_system(n=1)
-
 
 plt.plot(odqd.grid, odqd.potential(odqd.grid))
 
@@ -57,10 +51,10 @@ tdcisd = TDCISD(odqd, verbose=True)
 tdcisd.compute_ground_state()
 tdcisd.set_initial_conditions()
 
-for J in range(1, len(tdcisd.ci.energies)):
-    if np.any(np.abs(tdcisd.ci.allowed_dipole_transition(J - 1, J)) > 1e-12):
-        print(f"J = {J}")
-        print(tdcisd.ci.energies[J] - tdcisd.ci.energies[J - 1])
+# for J in range(1, len(tdcisd.ci.energies)):
+#     if np.any(np.abs(tdcisd.ci.allowed_dipole_transition(J - 1, J)) > 1e-12):
+#         print(f"J = {J}")
+#         print(tdcisd.ci.energies[J] - tdcisd.ci.energies[J - 1])
 
 background = odqd.potential(odqd.grid)
 
